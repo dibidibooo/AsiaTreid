85ff0469c178da526f8acf8b746cea3eca1e357a
dibidibooo <alaska.lubit.tebya@gmail.com>
2022-03-17 14:42:47 +0600

    17.03.2022..14:42

--- components/shared/footers/modules/FooterTop.vue
+++ components/shared/footers/modules/FooterTop.vue
@@ -3,13 +3,13 @@
         <div :class="{'container': container}">
             <div class="row m-0">
                 <div class="col-12 col-sm-4 p-0">
-                    <p class="text-center"><nuxt-link class="ps-footer__link" to="/"><i class="icon-wallet"></i>100% Money back</nuxt-link></p>
+                    <p class="text-center"><nuxt-link class="ps-footer__link" to="/"><i class="icon-wallet"></i>100% возврат денег</nuxt-link></p>
                 </div>
                 <div class="col-12 col-sm-4 p-0">
-                    <p class="text-center"><nuxt-link class="ps-footer__link" to="/"><i class="icon-bag2"></i>Non-contact shipping</nuxt-link></p>
+                    <p class="text-center"><nuxt-link class="ps-footer__link" to="/"><i class="icon-bag2"></i>Бесконтактная доставка</nuxt-link></p>
                 </div>
                 <div class="col-12 col-sm-4 p-0">
-                    <p class="text-center"><nuxt-link class="ps-footer__link" to="/"><i class="icon-truck"></i>Free delivery for order over $200</nuxt-link></p>
+                    <p class="text-center"><nuxt-link class="ps-footer__link" to="/"><i class="icon-truck"></i>Бесплатная доставка по договоренности</nuxt-link></p>
                 </div>
             </div>
         </div>
